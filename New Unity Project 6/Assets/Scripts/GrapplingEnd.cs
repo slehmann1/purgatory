b744bb43aedd6e7671d6fa50765ff93cc248d625
slehmann1 <samueljlehmann@gmail.com>
2014-09-23 19:21:21 -0600

    edited laser end

--- New Unity Project 6/Assets/Scripts/GrapplingEnd.cs
+++ New Unity Project 6/Assets/Scripts/GrapplingEnd.cs
@@ -8,6 +8,8 @@ public class GrapplingEnd : MonoBehaviour {
     private Quaternion rot, initRot;
     private bool contacted;
     private DistanceJoint2D dist;
+    private ParticleSystem parts;
+    public bool rotationEnabled;
     public void setRange(float range) {
         dist.distance=range;
     }
@@ -31,12 +33,19 @@ public class GrapplingEnd : MonoBehaviour {
         initRot=transform.rotation;
         dist.enabled=true;
         transform.rotation=initRot;
+        parts.active=true;
     }
     public void deactivate() {
-        coll.enabled=false;
-        renderer.enabled=false;
-        active=false;
-        dist.enabled=false;
+        try {
+            coll.enabled=false;
+            renderer.enabled=false;
+            active=false;
+            dist.enabled=false;
+            parts.active=false;
+        }
+        catch {
+            
+        }
        // rigidbody2D.isKinematic=false;
     }
     public void Setup(GameObject newStem) {
@@ -47,6 +56,7 @@ public class GrapplingEnd : MonoBehaviour {
         dist.connectedBody=transform.parent.transform.parent.rigidbody2D;
         dist.enabled=false;
         transform.parent=stem.transform.parent.transform.parent;
+        parts=GetComponent<ParticleSystem>();
     }
     // Use this for initialization
     void Start() {
@@ -55,22 +65,28 @@ public class GrapplingEnd : MonoBehaviour {
         deactivate();
     }
     void UpdatePos() {
-        if (!contacted) {//ifn the end isnt fixed-> set the pos/rot to the end of the stem
-            position=stem.transform.TransformPoint(new Vector2(0.5f, 0f));//set the world position to the local position of 0.5,0.5 in the bases space
-            rot=stem.transform.rotation;
+        try {
+            if (!contacted) {//ifn the end isnt fixed-> set the pos/rot to the end of the stem
+                position=stem.transform.TransformPoint(new Vector2(0.5f, 0f));//set the world position to the local position of 0.5,0.5 in the bases space
+                if (rotationEnabled) { 
+                rot=stem.transform.rotation;
+                    }
+            }
         }
+        catch { }
     }
     void OnCollisionEnter2D(Collision2D collision) {
-        float rotation=Mathf.Atan2(collision.contacts [0].normal.y, (collision.contacts [0].normal.x));//not sure on this,  should calculate the angle in relation to the collider7
-        rotation*=180;
-        rotation/=Mathf.PI;
-        //Debug.Log(rotation);
-        rot=Quaternion.Euler(new Vector3(0f, 0f, rotation));
-        
+        if (rotationEnabled) {
+            float rotation=Mathf.Atan2(collision.contacts [0].normal.y, (collision.contacts [0].normal.x));//not sure on this,  should calculate the angle in relation to the collider7
+            rotation*=180;
+            rotation/=Mathf.PI;
+            //Debug.Log(rotation);
+            rot=Quaternion.Euler(new Vector3(0f, 0f, rotation));
+        }
         transform.parent=null;
         rigidbody2D.isKinematic=true;
         transform.localRotation=Quaternion.identity;
-        if (!contacted) {//if it has simply changed length, will not update the rotation
+        if (!contacted&&rotationEnabled) {//if it has simply changed length, will not update the rotation
             transform.rotation=rot*initRot;
         }
         
@@ -80,7 +96,9 @@ public class GrapplingEnd : MonoBehaviour {
     void Update() {
         if (!contacted) {
             UpdatePos();
-            transform.rotation=rot*initRot;
+            if (rotationEnabled) {
+                transform.rotation=rot*initRot;
+            }
         }
         else {
         }
