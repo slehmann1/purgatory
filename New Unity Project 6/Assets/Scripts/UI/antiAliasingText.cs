c474f337d05b60263a43317fc4cbe0a599065ea2
slehmann1 <samueljlehmann@gmail.com>
2014-11-10 15:26:36 -0700

    Further UI work

---
+++ New Unity Project 6/Assets/Scripts/UI/antiAliasingText.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class antiAliasingText : MonoBehaviour {
+    private Text t;
+    private int step;
+    void Start() {
+        t=GetComponent<Text>();
+    }
+    private void setText() {
+        string s="Off";
+        switch (step) {
+            case 1:
+                s="2";
+                break;
+            case 2:
+                s="4";
+                break;
+            case 3:
+                s="8";
+                break;
+        }
+        if (step!=0) {
+            s+=" times";
+        }
+        t.text=s;
+
+
+    }
+    public void setAliasing(float i) {
+        step=(int)i;
+        setText();
+    }
+}
--- New Unity Project 6/Assets/Scripts/UI/textAppender.cs
+++ New Unity Project 6/Assets/Scripts/UI/textAppender.cs
@@ -2,19 +2,20 @@
 using UnityEngine;
 using System.Collections;
 [RequireComponent(typeof(Text))]
-public class percentageScript : MonoBehaviour {
+public class textAppender : MonoBehaviour {
     private Text t;
-    private float volume;
+    private float value;
+    public string appender;
     void Start() {
         t=GetComponent<Text>();
     }
     private void setText() {
-        t.text=Mathf.Round(volume)+" %";
+        t.text=Mathf.Round(value)+appender;
          
 
     }
-    public void setVolume(float i) {
-        volume=i;
+    public void setValue(float i) {
+        value=i;
         setText();
     }
 }
