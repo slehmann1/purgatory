3c0058f2cae3cb7861462741f119f923f7d0777a
slehmann1 <samueljlehmann@gmail.com>
2014-11-10 14:14:33 -0700

    Started UI work
    For main Menu+options menu

---
+++ New Unity Project 6/Assets/Scripts/percentageScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+using System.Collections;
+[RequireComponent(typeof(Text))]
+public class percentageScript : MonoBehaviour {
+    private Text t;
+    private float volume;
+    void Start() {
+        t=GetComponent<Text>();
+    }
+    private void setText() {
+        t.text=Mathf.Round(volume)+" %";
+         
+
+    }
+    public void setVolume(float i) {
+        volume=i;
+        setText();
+    }
+}
