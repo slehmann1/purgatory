1e86cb041cf048f3b3e662675f08900c8416698a
slehmann1 <samueljlehmann@gmail.com>
2014-11-11 18:01:42 -0700

    worked on gameCreation/saving
    implemented at the most basic level

---
+++ New Unity Project 6/Assets/Scripts/playerSave.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System;
+
+
+[Serializable]
+public class playerSave  {
+    private string nameOfFile;
+    public string fileName {
+        get { return nameOfFile; }
+        set { nameOfFile=value; }
+    }
+    
+}
---
+++ New Unity Project 6/Assets/Scripts/saveManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using UnityEngine.UI;
+using System;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public class saveManager : MonoBehaviour {
+    public Text t;
+
+    public void createNewGame() {
+        string s=t.text;
+        s=s.Replace(' ','_');
+        foreach(char c in Path.GetInvalidFileNameChars()){
+            s=s.Replace(c,'_');
+            Debug.Log(c);
+        }
+      
+        BinaryFormatter bf=new BinaryFormatter();
+        FileStream file=File.Create(Application.persistentDataPath+"/" +s+".save");
+        playerSave save=new playerSave();
+        save.fileName=s;
+        bf.Serialize(file,save);
+    }
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
