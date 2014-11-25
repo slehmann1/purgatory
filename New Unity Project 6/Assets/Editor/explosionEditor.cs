a100560d5c037b1cc2a578cdc9df5a5f45fba017
slehmann1 <s.lehmann@LAB054-205-05.EDU.epsb.ca>
2014-11-25 11:24:45 -0700

    started explosion work very initial

---
+++ New Unity Project 6/Assets/Editor/explosionEditor.cs
@@ -0,0 +1,21 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System.Collections.Generic;
+[CustomEditor (typeof (explosionForce))]
+[CanEditMultipleObjects]
+
+public class explosionEditor : Editor {
+	private explosionForce obj;
+	void Start(){
+		Debug.Log ("RUN");
+		obj = (explosionForce)target;
+		}
+	void OnSceneGUI() {
+		obj = (explosionForce)target; 
+		if (obj.range < 0) {
+						obj.range = 0;
+				}
+		Handles.DrawSolidDisc (obj.transform.position,  new Vector3(0, 0, 1), obj.range); 	
+
+}
+}	
---
+++ New Unity Project 6/Assets/Scripts/explosionForce.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class explosionForce : MonoBehaviour {
+	public float range;
+	private float slope;
+	public float maxForce;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	void getSlope(){
+		//f(x) = (-maxforce/maxrange)(x)+maxforce
+		//where f(x) is the force at a certain distance or x value
+		//this is  a linear function where the maxforce is at x = 0, and at x = max range f(x)=0
+		//thus maxforce is the yintercept and maxrange is the x intercept
+		slope = -maxForce / range;//slope = rise over run
+		}
+	float getForce(float distance){
+		return distance*slope+maxForce;//f(x) = (-maxforce/maxrange)(x)+maxforce, (-maxforce/maxrange) = slope
+		}
+}
