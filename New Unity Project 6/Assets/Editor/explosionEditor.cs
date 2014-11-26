d9e55c2d656b2573be5d1ed3fbbf57020ee467d1
slehmann1 <samueljlehmann@gmail.com>
2014-11-25 21:42:56 -0700

    Explosions finished with linear functions
    Will probably want to change it to something like y = 2^-x as a linear
    function is not providing the best result

--- New Unity Project 6/Assets/Editor/explosionEditor.cs
+++ New Unity Project 6/Assets/Editor/explosionEditor.cs
@@ -1,34 +1,41 @@
 ﻿using UnityEditor;
 using UnityEngine;
 using System.Collections.Generic;
-[CustomEditor (typeof (explosionForce))]
+[CustomEditor(typeof(explosionForce))]
 [CanEditMultipleObjects]
 
-public class explosionEditor : Editor {
-	private explosionForce obj;
-	private float oldRange = 0;
-	private Color c;
-	void Start(){
-		obj = (explosionForce)target;
-		c = Color.red;
-		c.a = 0.05f;
-		}
-	void OnSceneGUI() {
-		if (obj == null) {
-			Start ();
-				}
-		if (obj.range < 0) {
-						obj.range = 0;
-				}
+public class explosionEditor : Editor
+{
+    private explosionForce obj;
+    private float oldRange = 0;
+    private Color c;
+    void Start()
+    {
+        obj = (explosionForce)target;
+        c = Color.red;
+        c.a = 0.05f;
+    }
+    void OnSceneGUI()
+    {
+        if (obj == null)
+        {
+            Start();
+        }
+        if (obj.range < 0)
+        {
+            obj.range = 0;
+        }
 
-		if (obj.range != oldRange) {
-						obj.GetComponent<CircleCollider2D> ().radius = obj.range;
-				}
+        if (obj.range != oldRange)
+        {
+            obj.GetComponent<CircleCollider2D>().radius = obj.range;
+            obj.GetComponent<CircleCollider2D>().isTrigger = true;
+        }
 
-		Handles.color = c;
+        Handles.color = c;
 
-		Handles.DrawSolidDisc (obj.transform.position,  new Vector3(0, 0, 1), obj.range); 	
-		oldRange = obj.range;
+        Handles.DrawSolidDisc(obj.transform.position, new Vector3(0, 0, 1), obj.range);
+        oldRange = obj.range;
 
+    }
 }
-}	
--- New Unity Project 6/Assets/Scripts/explosionForce.cs
+++ New Unity Project 6/Assets/Scripts/explosionForce.cs
@@ -1,23 +1,68 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 [RequireComponent(typeof(CircleCollider2D))]
-public class explosionForce : MonoBehaviour {
+public class explosionForce : MonoBehaviour
+{
 
-	public float range;
-	private float slope;
-	public float maxForce;
-	// Use this for initialization
-	void Start () {
-	
-	}
-	void getSlope(){
-		//f(x) = (-maxforce/maxrange)(x)+maxforce
-		//where f(x) is the force at a certain distance or x value
-		//this is  a linear function where the maxforce is at x = 0, and at x = max range f(x)=0
-		//thus maxforce is the yintercept and maxrange is the x intercept
-		slope = -maxForce / range;//slope = rise over run
-		}
-	float getForce(float distance){
-		return distance*slope+maxForce;//f(x) = (-maxforce/maxrange)(x)+maxforce, (-maxforce/maxrange) = slope
-		}
+    public float range;
+    private float slope;
+    public float maxForce;
+    private List<Rigidbody2D> currentlyColliding;
+
+    public void Explode()
+    {
+        Debug.Log("RUN");
+        foreach(Rigidbody2D rigBody in currentlyColliding){
+            rigBody.AddForce(getForce(rigBody));
+        }
+    }
+    private Vector2 getForce(Rigidbody2D rigBody)
+    {
+        float xForce = getForce(rigBody.transform.position.x- transform.position.x);
+        float divisor = (rigBody.transform.position.x- transform.position.x)/xForce;//uses similar triangles, could alternatively use trig, but this is likely faster
+        return new Vector2(xForce,(getForce(rigBody.transform.position.y- transform.position.y)/divisor));
+    }
+
+    void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            Explode();
+        }
+    }
+    void Start()
+    {
+        currentlyColliding = new List<Rigidbody2D>();
+    }
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if(coll.rigidbody2D){
+            currentlyColliding.Add(coll.rigidbody2D);
+        }
+    else
+        {
+            Debug.LogWarning("Game object: "+coll.name+" does not contain a rigidbody2d to apply an explosive force to.");
+            currentlyColliding.Remove(coll.rigidbody2D);
+        }
+    }
+    void OnTriggerExit2D(Collider2D coll)
+    {
+        if (coll.rigidbody2D)
+        {
+            currentlyColliding.Remove(coll.rigidbody2D);
+        }
+    }
+    void getSlope()
+    {
+        //f(x) = (-maxforce/maxrange)(x)+maxforce
+        //where f(x) is the force at a certain distance or x value
+        //this is  a linear function where the maxforce is at x = 0, and at x = max range f(x)=0
+        //thus maxforce is the yintercept and maxrange is the x intercept
+        slope = -maxForce / range;//slope = rise over run
+    }
+    float getForce(float distance)
+    {
+        return distance * slope + maxForce;//f(x) = (-maxforce/maxrange)(x)+maxforce, (-maxforce/maxrange) = slope
+    }
 }
