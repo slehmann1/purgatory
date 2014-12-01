0e857072c8d68c5afab656be8d668540f89a3e51
slehmann1 <samueljlehmann@gmail.com>
2014-11-30 21:54:45 -0700

    Fixed seths errors

---
+++ New Unity Project 6/Assets/Scripts/laserConnector.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class laserConnector : MonoBehaviour {
+    public Transform endPoint, startPoint;
+	// Update is called once per frame
+	void Update () {
+        try
+        {
+            endPoint = GameObject.Find("LaserEnd").transform;
+            startPoint = transform.parent;
+            Debug.Log("DONE");
+            transform.position = startPoint.position + ((endPoint.position - startPoint.position) / 2);
+        }
+        catch { }
+	}
+}
