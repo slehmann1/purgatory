2d3b72c773717da8bae3f6cffdb744bf03917884
slehmann1 <samueljlehmann@gmail.com>
2015-01-05 20:41:42 -0700

    stuff done been has

---
+++ New Unity Project 6/Assets/adjustToScreenSize.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class adjustToScreenSize : MonoBehaviour {
+    public Camera cam;
+	// Use this for initialization
+	void Start () {
+        float height = cam.orthographicSize ;
+        float width = height * Screen.width / Screen.height;
+        transform.localScale = new Vector3(width/renderer.bounds.extents.x, height/renderer.bounds.extents.y, 0.1f);
+	}
+
+	
+}
